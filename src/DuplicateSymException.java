184cda119e16ba66854c303ec95130724df6cba6
Martin Wickham <spexguy070@gmail.com>
2015-09-18 12:29:05 -0500

    Remove P1, add P2 and JLex

--- src/DuplicateSymException.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Martin Wickham on 9/9/2015.
- */
-public class DuplicateSymException extends Exception {
-    public DuplicateSymException() { super(); }
-    public DuplicateSymException(String msg) { super(msg); }
-    public DuplicateSymException(String msg, Throwable cause) { super(msg, cause); }
-    public DuplicateSymException(Throwable cause) { super(cause); }
-}
--- src/EmptySymTableException.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Martin Wickham on 9/9/2015.
- */
-public class EmptySymTableException extends Exception {
-    public EmptySymTableException() { super(); }
-    public EmptySymTableException(String msg) { super(msg); }
-    public EmptySymTableException(String msg, Throwable cause) { super(msg, cause); }
-    public EmptySymTableException(Throwable cause) { super(cause); }
-}
